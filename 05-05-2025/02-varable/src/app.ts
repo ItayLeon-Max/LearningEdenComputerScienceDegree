66c22839196fb2a115bf7b92137f66616894db87
Itay Leon <itayleon81@walla.com>
2025-05-05 23:13:44 +0300

    feat: initialize TypeScript project with basic setup and example code
    - Added package.json with nodemon as a dependency and dev script for TypeScript execution.
    - Created package-lock.json to lock dependencies.
    - Added tsconfig.json for TypeScript configuration with strict type checking.
    - Implemented src/app.ts with examples of TypeScript variable declarations and console outputs.
    - Removed unnecessary .DS_Store files.

---
+++ 05-05-2025/02-varable/src/app.ts
@@ -0,0 +1,52 @@
+// this is a new code with typescript
+
+//input
+
+// let / const 
+// let <name>: <type> = <value>
+// const <name>: <type> = <value>
+
+
+//let 
+// number
+let eden: number = 10;
+// console.log(eden);
+eden = 20;
+console.log(eden);
+
+
+// string - 
+let edenName: string = "Eden";
+
+// boolean - true / false
+let isEden: boolean = true;
+
+// // any - any type
+// let anyType: any = 10; // any type = number
+// anyType = "Eden"; // any type = string
+
+// const - primitive type
+const number: number = 10;
+// number = 20; // error: cannot reassign a constant variable
+
+// output
+// console.log()
+
+// id:
+const yourName: string = "Eden";
+const yourAge: number = 25;
+const yourIsEden: boolean = true;
+
+console.log(`my name is: ${yourName}, my age is: ${yourAge}, am i Eden: ${yourIsEden}`);
+
+const API_URL: string = "https://api.example.com";
+const printAPI = API_URL;
+console.log(`my api url is: ${API_URL}`);
+
+const a: number = 10;
+const b: number = 5;
+const sum: number = a + b;
+
+
+
+
