f3c414a231ec66802716b460b40c656d0d32ce43
Itay Leon <itayleon81@walla.com>
2025-05-15 14:08:20 +0300

    Initialize project with TypeScript setup and nodemon for development
    - Created package.json with nodemon as a dependency and added dev script for TypeScript execution.
    - Added package-lock.json to lock dependencies.
    - Created tsconfig.json with compiler options for TypeScript.
    - Added initial app.ts file for application entry point.
    - Removed unnecessary .DS_Store files.

--- 12-05-2025/01-condition/src/app.ts
+++ 12-05-2025/01-condition/src/app.ts
@@ -34,6 +34,7 @@ if(foo != 5){
     console.log("foo is SMALL than 5");
 }
 
+// get max number with if else:
 const foo1: number = 578;
 const foo2: number = 6;
 const foo3: number = 100;
@@ -54,11 +55,23 @@ if(foo1 > foo2){ // 57 > 6 == true
 
 // || = OR , && = AND
 // syntax: if(condition1 || condition2) { ... } : if(condition1 && condition2) { ... }
-const foo4: number = 5;
-const foo5: number = 6;
-if((foo4 > foo5) || (foo4 < foo5)){}
+const foo4: number = 6;
+const foo5: number = 5;
+if((foo4 > foo5) || (foo4 < foo5)){
+    //enter code here
+} else {
+    //enter code here
+}
 
-if((foo4 > foo5) && (foo4 < foo5)){}
+if((foo4 > foo5) && (foo4 < foo5)){
+    //enter code here
+} 
 
+// shorthand if
+// syntax: condition ? block : else
+
+const foo6: number = 5;
+const result = foo6 >= 5 ? "foo6 is BIG than 5" /*true */ : "foo6 is SMALL than 5" /*false */ ;
+console.log(result);
 
 
