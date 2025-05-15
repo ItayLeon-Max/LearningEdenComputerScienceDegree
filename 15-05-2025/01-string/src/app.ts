ca2b4fb65329f83e1200d7a43846402c7168e578
Itay Leon <itayleon81@walla.com>
2025-05-15 14:19:28 +0300

    end string

--- 15-05-2025/01-string/src/app.ts
+++ 15-05-2025/01-string/src/app.ts
@@ -1,1 +1,30 @@
 //string:
+/*
+    string functions:
+    - stringLength: .length()
+    - stringUpperCase: .toUpperCase()
+    - stringLowerCase: .toLowerCase()
+    - stringSplit: .split()
+ */
+
+    // Eden ---> ['E' 'd' 'e' 'n']
+    //             0   1   2   3
+
+    // .length()
+    const str: string = "Eden";
+    console.log(str.length)
+
+    // .toUpperCase()
+    console.log(str.toUpperCase())
+
+    // .toLowerCase()
+    console.log(str.toLowerCase())
+
+    // .split()
+    console.log(str.split(''))
+
+    const str2: string = "Eden is a software engineer";
+    console.log(str2[1])
+    console.log(str2.length)
+    console.log(str2[24])
+    console.log(str2[4])
